892046d4454abac263fa18dbe2368e3d899ceefb
Olivia-L-V <80695018+Olivia-L-V@users.noreply.github.com>
2021-05-05 09:56:41 +0200

    bm

--- SpQRt/imports/burgerMenu/bm.js
+++ SpQRt/imports/burgerMenu/bm.js
@@ -1,8 +1,40 @@
 import './bm.html';
+
 import '../entrainements/entrainements.js';
+import '../recherche/recherche.js';
+import '../contact/contact.js';
+import '../apropos/apropos.js';
+import '../profil/profil.js';
+import '../carte/carte.js';
+
 Template.app_body.events({
     'click #entrainements' (event) {
         event.preventDefault();
         FlowRouter.go('entrainements');
+    },
+
+    'click #recherche' (event) {
+        event.preventDefault();
+        FlowRouter.go('recherche');
+    },
+
+    'click #contact' (event) {
+        event.preventDefault();
+        FlowRouter.go('contact');
+    },
+
+    'click #apropos' (event) {
+        event.preventDefault();
+        FlowRouter.go('apropos');
+    },
+
+    'click #profil' (event) {
+        event.preventDefault();
+        FlowRouter.go('profil');
+    },
+
+    'click #carte' (event) {
+        event.preventDefault();
+        FlowRouter.go('carte');
     },
 });

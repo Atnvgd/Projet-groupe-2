1feeb1a27d4ee47549c5e2f72a1ba987b783747a
Atnvgd <80701203+Atnvgd@users.noreply.github.com>
2021-05-10 17:42:30 +0200

    creercompte

--- SpQRt/imports/creercompte/creercompte.js
+++ SpQRt/imports/creercompte/creercompte.js
@@ -1,1 +1,13 @@
 import './creercompte.html';
+
+Template.creercompte.events({
+
+    'click #creercompte' (event) {
+        event.preventDefault();
+        let nom = document.getElementById('username').value;
+        let mail = document.getElementById('email').value;
+        let mdp = document.getElementById('password').value;
+        let mdp2 = document.getElementById('password2').value;
+    },
+
+})

41099a54f9a06f52222ff3b125d5bdaaa469b210
Atnvgd <80701203+Atnvgd@users.noreply.github.com>
2021-05-10 22:22:17 +0200

    mdp

--- SpQRt/imports/creercompte/creercompte.js
+++ SpQRt/imports/creercompte/creercompte.js
@@ -15,9 +15,13 @@ Template.creercompte.events({
         let mail = document.getElementById('email').value;
         let mdp = document.getElementById('password').value;
         let mdp2 = document.getElementById('password2').value;
-        if (mdp.length > 5) {
+        let pUser = document.getElementById('pUser');
+        let pMail = document.getElementById('pMail');
+        let pMdp = document.getElementById('pMdp');
+        
+        if (nom != '' && mdp != '' && mail != '') {
             if (mdp == mdp2) {
-                if(nom != '' && mdp != '' && mail != '') {
+                if(mdp.length > 5) {
                     Accounts.createUser({
                         username: nom,
                         password: mdp,
@@ -31,13 +35,22 @@ Template.creercompte.events({
                         }
                     });
                 } else{
-                    alert('Veuillez remplir les champs!');
+                    event.preventDefault();
+                    pMdp.innerHTML = 'Mot de passe trop court!';
+                    pMail.innerHTML = '';
+                    pUser.innerHTML = '';
                 }
             } else{
-                alert('Vos mots de passe ne correspondent pas!');
+                event.preventDefault();
+                pMdp.innerHTML = 'Vos mots de passe ne correspondent pas!';
+                pMail.innerHTML = '';
+                pUser.innerHTML = '';
             }
         } else{
-            alert('Votre mot de passe est trop court!');
+            event.preventDefault();
+            pMdp.innerHTML = 'Veuillez remplir les champs!';
+            pMail.innerHTML = 'Veuillez remplir les champs!';
+            pUser.innerHTML = 'Veuillez remplir les champs!';
         }
     },
 

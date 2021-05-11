d3f1baebafb8eb8868b1458dc8b4c8480c27634e
Atnvgd <80701203+Atnvgd@users.noreply.github.com>
2021-05-11 13:39:13 +0200

    pmessage

--- SpQRt/imports/connection/connection.js
+++ SpQRt/imports/connection/connection.js
@@ -11,9 +11,10 @@ Template.connexion.events({
         event.preventDefault();
         let mail = document.getElementById('email').value;
         let mdp = document.getElementById('password').value;
+        let pMessage = document.getElementById('pMessage');
         Meteor.loginWithPassword(mail, mdp, (error) => {
             if (error){
-                alert(error.message)
+                pMessage.innerHTML = `Nom d'utilisateur ou mot de passe incorrect.`;
             } else{
                 SetTimeout(() => FlowrouterGo('apropos'), 200); 
             }

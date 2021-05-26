4c7f21b48e01fbb914cd586222c877d077c8e3e8
lauremargot <laure.margot@bluewin.ch>
2021-05-26 10:29:42 +0200

    Update exercicesTout.js

--- SpQRt/imports/entrainements/exercicesTout/exercicesTout.js
+++ SpQRt/imports/entrainements/exercicesTout/exercicesTout.js
@@ -15,23 +15,8 @@ Template.exercicesTout.helpers({
     }
 })
 
-const structure = {
-    titre: "L'exercice des burpees",
-    muscle: "Une grande partie des muscles du corps",
-    difficulte: 5,
-    description: `Placez-vous debout les pieds légèrement écartés. <br>
-        Descendez en pliant vos genoux (comme pour faire un squat) puis posez vos mains sur le sol. <br>
-        Lancez vos jambes derrière vous et gardez vos bras tendus. <br>
-        Votre corps doit faire une ligne droite. <br>
-        Descendez en pliant vos coudes (comme pour faire une pompe). <br>
-        Puis remontez en tendant le bras. <br>
-        Ensuite ramenez vos pieds sous votre corps. <br>
-        Finalement sautez verticalement les bras tendus vers le ciel. <br>
-        Répétez l'exercice plusieurs fois. <br>`,
-    image: "https://i.pinimg.com/originals/c4/cd/3d/c4cd3de835c9d3df08c6a976f315b5b9.jpg",
-    qrCode: "lien vers le QR code",
-    type: "tout"
-}
+db.exercices.insert({titre: "Un entraînement circuit training",muscle: "Une grande partie des muscles du corps",difficulte: 4,duree: "7 min 30 le tour",description: `Suivez les exercices proposés sur l'image. <br>Effectuez chaque exercice pendant 30 secondes. <br>Entre chaque exercice vous pouvez prendre une pause de 10 secondes. <br>Si vous voulez faire plusieurs séries du circuit à la suite, vous pouvez prendre une pause de 2 minutes entre chaque série. <br>`,image: "https://www.epsregal.fr/wp-content/uploads/2020/03/eps-continuite-pedagogique.jpg",qrCode: "https://i.imgur.com/HhjBlC0.png ",type: "tout",});
+
 
 Template.app_body.events({
     'click #exercice2' (event) {

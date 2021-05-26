1bc11e19cffc7f46b7b6a8523de462ebc25710c4
Atnvgd <80701203+Atnvgd@users.noreply.github.com>
2021-05-26 11:20:46 +0200

    slt

--- SpQRt/imports/entrainements/exercicesTout/exercicesTout.js
+++ SpQRt/imports/entrainements/exercicesTout/exercicesTout.js
@@ -15,9 +15,6 @@ Template.exercicesTout.helpers({
     }
 })
 
-db.exercices.insert({titre: "Un entraînement circuit training",muscle: "Une grande partie des muscles du corps",difficulte: 4,duree: "7 min 30 le tour",description: `Suivez les exercices proposés sur l'image. <br>Effectuez chaque exercice pendant 30 secondes. <br>Entre chaque exercice vous pouvez prendre une pause de 10 secondes. <br>Si vous voulez faire plusieurs séries du circuit à la suite, vous pouvez prendre une pause de 2 minutes entre chaque série. <br>`,image: "https://www.epsregal.fr/wp-content/uploads/2020/03/eps-continuite-pedagogique.jpg",qrCode: "https://i.imgur.com/HhjBlC0.png ",type: "tout",});
-
-
 Template.app_body.events({
     'click #exercice2' (event) {
         event.preventDefault();
